e342ccbae67cff8cc86a623b5e0cf6455d108539
Craig Sloggett <craigsloggett@gmail.com>
2025-01-23 16:39:42 -0500

    wip: Stopping point

--- vcs.tf
+++ vcs.tf
@@ -1,5 +1,5 @@
 resource "tfe_oauth_client" "test" {
-  name                = "my-github-oauth-client"
+  name                = "github-oauth-client"
   organization        = "my-org-name"
   api_url             = "https://api.github.com"
   http_url            = "https://github.com"
