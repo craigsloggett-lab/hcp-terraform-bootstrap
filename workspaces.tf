85fcabecf8bb98a631a06fa256460bd279a351f7
Craig Sloggett <craigsloggett@gmail.com>
2025-01-27 20:19:44 -0500

    chore: Remove unrelated workspace configuration

--- workspaces.tf
+++ workspaces.tf
@@ -1,15 +1,1 @@
 # The resources for the workspace configured in `backend.tf` can be found in `bootstrap.tf`.
-
-resource "tfe_workspace" "terraform_aws_tfe_fdo_docker_active_active" {
-  name         = "terraform-aws-tfe-fdo-docker-active-active"
-  organization = tfe_organization.this.name
-  project_id   = tfe_project.backend.id
-
-  terraform_version = var.terraform_version
-  queue_all_runs    = true
-
-  vcs_repo {
-    identifier     = "${var.github_organization_name}/terraform-aws-tfe-fdo-docker-active-active"
-    oauth_token_id = data.tfe_oauth_client.github.oauth_token_id
-  }
-}
