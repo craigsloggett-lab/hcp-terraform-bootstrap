af51aaccf6bc3fc120b895da0047533dc7c02ef9
Craig Sloggett <craigsloggett@gmail.com>
2025-01-27 20:38:20 -0500

    wip: Stopping point

--- registry.tf
+++ registry.tf
@@ -1,3 +1,13 @@
 data "tfe_registry_gpg_keys" "all" {
   organization = tfe_organization.this.id
 }
+
+# Add the following providers:
+# - AWS
+# - TFE
+# - HTTP
+# - Random
+
+# Add the following modules:
+# - AWS VPC
+# - My TFE AWS one
