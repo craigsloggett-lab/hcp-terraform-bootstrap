6521e9d0e9077c34fa91d5c3f135a7ab4323677d
Craig Sloggett <craigsloggett@gmail.com>
2025-01-28 20:32:45 -0500

    feat: Add tfe module workspace

--- workspaces.tf
+++ workspaces.tf
@@ -1,1 +1,16 @@
 # The resources for the workspace configured in `backend.tf` can be found in `bootstrap.tf`.
+
+resource "tfe_workspace" "aws_tfe_fdo_docker_active_active_module" {
+  name         = "terraform-aws-tfe-fdo-docker-active-active"
+  organization = tfe_organization.this.name
+  project_id   = tfe_project.modules.id
+
+  auto_apply        = true
+  queue_all_runs    = true
+  terraform_version = var.terraform_version
+
+  vcs_repo {
+    identifier     = "${var.github_organization_name}/terraform-aws-tfe-fdo-docker-active-active"
+    oauth_token_id = data.tfe_oauth_client.github.oauth_token_id
+  }
+}
