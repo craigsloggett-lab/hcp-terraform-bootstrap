252a854b49d8f02c4beddee230cf7deae9b752ca
Craig Sloggett <craigsloggett@gmail.com>
2025-10-15 20:24:49 -0400

    feat: Add Terraform provider credentials variable sets

--- variable_sets.tf
+++ variable_sets.tf
@@ -1,8 +1,10 @@
 # The resources for the variable set containing authentication for the TFE provider can be found in `bootstrap.tf`.
 
+# AWS Provider Credentials
+
 resource "tfe_variable_set" "aws_provider_authentication" {
   name         = var.aws_provider_authentication_variable_set_name
-  description  = "The tokens used to authenticate the AWS provider."
+  description  = "The secrets used to authenticate the AWS Provider."
   organization = tfe_organization.this.name
 }
 
@@ -14,4 +16,20 @@ resource "tfe_project_variable_set" "modules" {
 resource "tfe_project_variable_set" "lab" {
   project_id      = tfe_project.lab.id
   variable_set_id = tfe_variable_set.aws_provider_authentication.id
+}
+
+# Azure Provider Credentials
+
+resource "tfe_variable_set" "azurerm" {
+  name         = var.azurerm_provider_authentication_variable_set_name
+  description  = "The secrets used to authenticate the Azure Provider."
+  organization = tfe_organization.this.name
+}
+
+# Microsoft Fabric Provider Credentials
+
+resource "tfe_variable_set" "fabric" {
+  name         = var.fabric_provider_authentication_variable_set_name
+  description  = "The secrets used to authenticate the Microsoft Fabric Provider."
+  organization = tfe_organization.this.name
 }
--- variables.tf
+++ variables.tf
@@ -64,10 +64,22 @@ variable "github_organization_name" {
   description = "The name of the GitHub organization used as the VCS provider."
 }
 
-# Module Configuration
+# Terraform Provider Configuration
 
 variable "aws_provider_authentication_variable_set_name" {
   type        = string
-  description = "The name of the variable set used to authenticate the AWS provider."
+  description = "The name of the variable set used to authenticate the AWS Provider."
   default     = "AWS Provider Authentication"
+}
+
+variable "azurerm_provider_authentication_variable_set_name" {
+  type        = string
+  description = "The name of the variable set used to authenticate the Azure Provider."
+  default     = "Azure Provider Authentication"
+}
+
+variable "fabric_provider_authentication_variable_set_name" {
+  type        = string
+  description = "The name of the variable set used to authenticate the Fabric Provider."
+  default     = "Microsoft Fabric Provider Authentication"
 }
