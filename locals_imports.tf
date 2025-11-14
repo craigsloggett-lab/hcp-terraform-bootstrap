08918f0b3eb3b543ea2c02bcd3f52dee2a7ad069
Craig Sloggett <craigsloggett@gmail.com>
2025-11-14 16:22:39 -0500

    Run the script to add necessary resources

--- locals_imports.tf
+++ locals_imports.tf
@@ -4,6 +4,7 @@
 locals {
   imports = {
     team_ids = {
+      admins          = "team-AgMKArPE1pnwYpPz"
       owners          = "team-p9ryLNrpQ9TNmEFh"
       wiz-integration = "team-E8EciwrWaKutyh7u"
     }
@@ -20,12 +21,15 @@ locals {
     project_ids = {
       Administration    = "prj-LMG8bhhEpUzg9rrb"
       "Default Project" = "prj-1ifijXyay7xeH9Zc"
+      Modules           = "prj-bQ2amwBJzTyARnL1"
+      Stacks            = "prj-FDhpzFrAfccvBno4"
     }
     variable_set_ids = {
       "TFE Provider Authentication" = "varset-FwFGK65suTr7eaAA"
     }
     workspace_ids = {
-      hcp-terraform-admin = "ws-NJNrsMM37W7AgWNx"
+      hcp-terraform-bootstrap = "ws-dAs6ozkxSPBE4Td9"
+      hcp-terraform-admin     = "ws-NJNrsMM37W7AgWNx"
     }
     oauth_client_ids = {
       craigsloggett-lab = "oc-uR6kEX1Xt6fqLhTB"
