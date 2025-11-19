3b2d1115c97cc2979b8681a064d2d788436c5288
Craig Sloggett <craigsloggett@gmail.com>
2025-11-19 15:29:15 -0500

    chore: Update examples

--- examples/backend.tf
+++ examples/backend.tf
@@ -1,10 +1,10 @@
 terraform {
   cloud {
-    organization = "craigsloggett-lab" # Update the `hcp_terraform_organization_name` variable to match.
+    organization = "craigsloggett-lab"
 
     workspaces {
-      project = "Administration"          # Update the `backend_project_name` variable to match.
-      name    = "terraform-tfe-bootstrap" # Update the `backend_workspace_name` variable to match.
+      project = "Administration"
+      name    = "terraform-tfe-bootstrap"
     }
   }
 }
--- examples/main.tf
+++ examples/main.tf
@@ -2,3 +2,35 @@ module "terraform_tfe_bootstrap" {
   source = "git::https://github.com/craigsloggett-lab/hcp-terraform-bootstrap?ref=restructure-as-module"
   #version = "0.9.5"
 }
+
+# The HCP Terraform organization.
+import {
+  id = module.terraform_tfe_bootstrap.hcp_terraform_organization_name
+  to = module.terraform_tfe_bootstrap.tfe_organization.this
+}
+
+# The members of the HCP Terraform organization.
+import {
+  for_each = module.terraform_tfe_bootstrap.hcp_terraform_organization_membership
+
+  id = each.key
+  to = module.terraform_tfe_bootstrap.tfe_organization_membership.this[each.key]
+}
+
+# The "owners" team.
+import {
+  id = "${module.terraform_tfe_bootstrap.hcp_terraform_organization_name}/owners"
+  to = module.terraform_tfe_bootstrap.tfe_team.owners
+}
+
+# The members of the "owners" team.
+import {
+  id = module.terraform_tfe_bootstrap.owners_team_id
+  to = module.terraform_tfe_bootstrap.tfe_team_organization_members.owners
+}
+
+# The "Default Project" project.
+import {
+  id = module.terraform_tfe_bootstrap.default_project_id
+  to = module.terraform_tfe_bootstrap.tfe_project.default
+}
