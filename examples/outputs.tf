ae17a705dd91b9464c5d8bb064b9b6ae5de562d7
Craig Sloggett <craigsloggett@gmail.com>
2025-11-19 20:17:48 -0500

    fix: Specify object as output

--- examples/outputs.tf
+++ examples/outputs.tf
@@ -1,5 +1,8 @@
 output "hcp_terraform_organization" {
-  value       = module.terraform_tfe_bootstrap.hcp_terraform_organization
+  value = {
+    id   = module.terraform_tfe_bootstrap.hcp_terraform_organization.id
+    name = module.terraform_tfe_bootstrap.hcp_terraform_organization.name
+  }
   description = "Details about the tfe_organization resource."
 }
 
