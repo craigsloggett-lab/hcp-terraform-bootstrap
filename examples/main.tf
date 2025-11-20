364a005281c6c0298238fceb28f0567ef67dfdf6
Craig Sloggett <craigsloggett@gmail.com>
2025-11-19 20:04:19 -0500

    wip: Testing

--- examples/main.tf
+++ examples/main.tf
@@ -1,6 +1,5 @@
 # This module requires no inputs, all of the "magic" can be seen in the `imports.tf` file.
 
 module "terraform_tfe_bootstrap" {
-  #source = "git::https://github.com/craigsloggett-lab/hcp-terraform-bootstrap?ref=v0.10.0"
-  source = "../"
+  source = "git::https://github.com/craigsloggett-lab/hcp-terraform-bootstrap?ref=restructure-as-module"
 }
