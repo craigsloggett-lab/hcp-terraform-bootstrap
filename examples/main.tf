2b09cafb48ed8946e16d2fba2787600bf09623f5
Craig Sloggett <craigsloggett@gmail.com>
2025-11-20 16:03:52 -0500

    test: Reference module from git source

--- examples/main.tf
+++ examples/main.tf
@@ -1,8 +1,8 @@
 # This module requires no inputs, all of the "magic" can be seen in the `imports.tf` file.
 
 module "terraform_tfe_bootstrap" {
-  #source = "git::https://github.com/craigsloggett-lab/hcp-terraform-bootstrap?ref=restructure-as-module"
-  source = "../"
+  source = "git::https://github.com/craigsloggett-lab/hcp-terraform-bootstrap?ref=restructure-as-module"
+  #source = "../"
 
   tfe_organization = {
     email = "craig.sloggett@hashicorp.com"
