bc34e76d8ade4f8cc04d91661ecbdba74e3cc913
Craig Sloggett <craigsloggett@gmail.com>
2025-11-19 20:44:39 -0500

    chore: Add outputs in example

--- examples/outputs.tf
+++ examples/outputs.tf
@@ -5,3 +5,8 @@ output "hcp_terraform_organization" {
   }
   description = "Details about the tfe_organization resource."
 }
+
+output "hcp_terraform_organization_inputs" {
+  value       = module.terraform_tfe_bootstrap.hcp_terraform_organization_inputs
+  description = "The input paramters for the HCP Terraform organization configuration."
+}
