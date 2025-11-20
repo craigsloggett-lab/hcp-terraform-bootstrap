728d8237c74f83f389ff09576a216d3700282e23
Craig Sloggett <craigsloggett@gmail.com>
2025-11-19 20:28:21 -0500

    chore: Use objects in outputs

--- examples/outputs.tf
+++ examples/outputs.tf
@@ -5,8 +5,3 @@ output "hcp_terraform_organization" {
   }
   description = "Details about the tfe_organization resource."
 }
-
-output "default_project_id" {
-  value       = module.terraform_tfe_bootstrap.default_project_id
-  description = "Details about the tfe_organization resource."
-}
