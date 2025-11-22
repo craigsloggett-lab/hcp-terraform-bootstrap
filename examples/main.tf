2b4755ca3b5ee4366e495beff90c1bd98f4237f8
Craig Sloggett <craigsloggett@gmail.com>
2025-11-21 20:53:34 -0500

    feat: Remove all resource blocks out from the module

--- examples/main.tf
+++ examples/main.tf
@@ -1,10 +1,40 @@
 module "terraform_tfe_bootstrap" {
-  source = "git::https://github.com/craigsloggett-lab/terraform-tfe-bootstrap?ref=v0.10.2"
+  # TODO: Reference the public registry once published.
+  source = "git::https://github.com/craigsloggett-lab/terraform-tfe-bootstrap?ref=v0.11.0"
+  #version = "X.X.X" # Add this parameter once it is published.
+}
 
-  # Optionally, override some or all of the default values
-  # for the `tfe_organization` resource.
-  tfe_organization = {
-    session_timeout_minutes = 480
-    cost_estimation_enabled = true
-  }
+data "tfe_organization" "this" {
+  name = "craigsloggett-lab"
+}
+
+resource "tfe_organization" "this" {
+  name  = data.tfe_organization.this.name
+  email = data.tfe_organization.this.email
+
+  # Organization configuration.
+  assessments_enforced = true
+}
+
+resource "tfe_organization_membership" "this" {
+  for_each = module.terraform_tfe_bootstrap.tfe_organization_membership
+
+  organization = tfe_organization.this.name
+  email        = each.value.email
+}
+
+resource "tfe_team" "owners" {
+  organization = tfe_organization.this.name
+  name         = "owners"
+}
+
+resource "tfe_team_organization_members" "owners" {
+  team_id                     = tfe_team.owners.id
+  organization_membership_ids = module.terraform_tfe_bootstrap.tfe_team.owners.organization_membership_ids
+}
+
+resource "tfe_project" "default" {
+  organization = tfe_organization.this.name
+  name         = "Default Project"
+  description  = "The default project for new workspaces."
 }
